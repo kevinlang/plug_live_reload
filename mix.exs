df25e3bdf0f29f92221d714cc858728578e30a6a
Kevin Lang <me@gnalck.com>
2021-08-13 12:21:22 -0400

    update mix

--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule PlugLiveReload.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/kevinlang/plug_live_reload"
+  @version "0.1.0"
+
   def project do
     [
       app: :plug_live_reload,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "Adds live-reload functionality for Plug during development.",
+      package: package(),
+
+      # Docs
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs()
     ]
   end
 
@@ -28,6 +40,27 @@ defmodule PlugLiveReload.MixProject do
       # {:jason, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Kevin Lang"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extras: [
+        "README.md"
+      ],
+      main: "readme"
     ]
   end
 end
