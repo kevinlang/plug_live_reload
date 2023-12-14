b807eedd457613d5600bb11a8224fee0ebf16813
Kevin Lang <me@gnalck.com>
2023-12-14 08:47:19 +0800

    bump deps and version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PlugLiveReload.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/kevinlang/plug_live_reload"
-  @version "0.1.1"
+  @version "0.2.0"
 
   def project do
     [
@@ -34,10 +34,10 @@ defmodule PlugLiveReload.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:file_system, "~> 0.2 or ~> 1.0"},
-      {:plug, "~> 1.12"},
-      {:cowboy, "~> 2.9"},
-      {:ex_doc, "~> 0.21", only: :docs, runtime: false}
+      {:file_system, "~> 1.0"},
+      {:plug, "~> 1.15"},
+      {:cowboy, "~> 2.10"},
+      {:ex_doc, "~> 0.31", only: :docs, runtime: false}
       # {:jason, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
